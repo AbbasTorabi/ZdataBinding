df1db06ac75594d76c4568b64459a5d209ac75ec
abbas <abbas.programing@gmail.com>
2019-02-28 10:19:16 +0330

    Create ZdataBinding library via pod

---
+++ Example/Pods/Target Support Files/Pods-ZdataBinding_Example/Pods-ZdataBinding_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ZdataBinding_Example : NSObject
+@end
+@implementation PodsDummy_Pods_ZdataBinding_Example
+@end
---
+++ Example/Pods/Target Support Files/ZdataBinding/ZdataBinding-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ZdataBinding : NSObject
+@end
+@implementation PodsDummy_ZdataBinding
+@end
